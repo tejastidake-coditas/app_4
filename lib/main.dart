84fc5b9daa45a47682520cb6d851f15cfd9465b9
Tejas Tidake <tejas.tidake@coditas.com>
2025-02-05 15:40:16 +0530

    Some changes

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,27 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("Handling user input"), backgroundColor: Colors.cyan,),
+        appBar: AppBar(
+          title: Text("Handling user input"),
+          backgroundColor: Colors.cyan,
+        ),
         body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
+          padding: const EdgeInsets.all(12.0),
+          child: ListView(
             children: [
               ElevatedButton(onPressed: () {}, child: Text("Elevated Button")),
               FilledButton(onPressed: () {}, child: Text("Filled Button")),
               OutlinedButton(onPressed: () {}, child: Text("Outlined Button")),
               TextButton(onPressed: () {}, child: Text("Text Button")),
               IconButton(onPressed: () {}, icon: Icon(Icons.add)),
-              FloatingActionButton(onPressed: () {}, child: Icon(Icons.add),),
-              
-              SelectableText("Hii there, this is Selectable Text, not just an normal/regular text.\n Here we can select this text by holding on it and get options menu")
+              FloatingActionButton(
+                onPressed: () {},
+                child: Icon(Icons.add),
+              ),
+              SelectableText(
+                  "Hii there, this is Selectable Text, not just an normal/regular text.\n Here we can select this text by holding on it and get options menu"),
+
+
             ],
           ),
         ),
